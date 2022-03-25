5a3326a428486f391089e802381b53d3bd1b6647
Ivan Volnov <iv.volnov@yandex.com>
2022-03-25 15:08:01 +0300

    project creation

---
+++ baraka/baraka/barakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  barakaApp.swift
+//  baraka
+//
+//  Created by Ivan Volnov on 25.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct barakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
